92bbd84b92f0797bfe3f6c783eced3a70c7dc905
Amanda Treutler <amandathedev@gmail.com>
2019-03-15 17:55:18 -0500

    New units

--- cooking_conversion.py
+++ cooking_conversion.py
@@ -4,5 +4,10 @@ cups = int(cups)
 
 tablespoons = cups * 16.66
 teaspoons = cups * 50
+pints = cups * 0.44
+quarts = cups * 0.264
+liters = cups * 0.25
+gallons = cups * 0.066
+fluid_ounces = cups * 8.333
 
-print(f"That's {tablespoons} tablespoons, {teaspoons} teaspoons")
+print(f"That's {tablespoons} tablespoons, {teaspoons} teaspoons, {pints} pints, {quarts} quarts, {liters} liters, {gallons} gallons, {fluid_ounces} fluid ounces")
