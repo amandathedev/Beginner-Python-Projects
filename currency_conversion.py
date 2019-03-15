d97eaf52f6ce45605168f97fc24da5306d76e2d3
Amanda Treutler <amandathedev@gmail.com>
2019-03-14 21:06:54 -0500

    Added more currencies

--- currency_conversion.py
+++ currency_conversion.py
@@ -12,4 +12,7 @@ dong = round(dong, 2)
 kip = float(dollar) * 8582
 kip = round(kip, 2)
 
-print("You have:\n " + str(euro) + " Euros\n " + str(baht) + " Baht\n " + str(dong) + " Dong\n " + str(kip) + " Kip")
+forint = float(dollar) * 278.1
+forint = round(forint, 2)
+
+print("You have:\n " + str(euro) + " Euros\n " + str(baht) + " Baht\n " + str(dong) + " Dong\n " + str(kip) + " Kip\n" + str(forint) + " Forint")
