bf7862f383e53b1fe6278ac2ef463348a90d44a3
Amanda Treutler <amandathedev@gmail.com>
2019-03-14 20:46:25 -0500

    Added additional currencies

--- currency_conversion.py
+++ currency_conversion.py
@@ -3,4 +3,13 @@ dollar = input("How many dollars do you have? ")
 euro = float(dollar) * 0.88
 euro = round(euro, 2)
 
-print("You have " + str(euro) + " in Euros.")
+baht = float(dollar) * 31.76
+baht = round(baht, 2)
+
+dong = float(dollar) * 23190
+dong = round(dong, 2)
+
+kip = float(dollar) * 8582
+kip = round(kip, 2)
+
+print("You have:\n " + str(euro) + " Euros\n " + str(baht) + " Baht\n " + str(dong) + " Dong\n " + str(kip) + " Kip")
